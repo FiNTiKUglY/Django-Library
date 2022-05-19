a9794f24a08cb5dd58b634aa46a30da1581ef932
FiNTiKUglY <ilya.pronin.0202@gmail.com>
2022-05-20 00:53:34 +0300

    rename package

--- lab3/manager/settings.py
+++ lab3/manager/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'library'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['hello/templates'],
+        'DIRS': ['library/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
